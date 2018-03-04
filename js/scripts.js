54a9667f3aece590e1d281ba158cdda0068ff06f
zecollokaris <collo.kariss@gmail.com>
2018-03-04 21:55:50 +0300

    java script added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-//If
+//If player 1 is playing player 2 can't and vice verse
 var player1_playing = true;
 var player2_playing = false;
 
-//
+//player 1
 function dice_1_clicked(){
   if (player1_playing == true) {
     var random1 = generate_random_number();
@@ -20,7 +20,10 @@ function dice_1_clicked(){
   }
 
 }
-//
+
+
+
+//player 2
 function dice_2_clicked(){
   if (player2_playing == true) {
     var random2 =  generate_random_number();
@@ -42,5 +45,23 @@ function dice_2_clicked(){
 
 
 function generate_random_number(){
-    return Math.floor(Math.random()*6) +1;
+    return Math.floor(Math.random()*6) +1 ;
+}
+
+//hold game function Player 1
+function holdgame1_clicked() {
+  if (player1_playing == true) {
+    console.log('Player 2 turn to play')
+    player1_playing = false;
+    player2_playing = true;
+  }
+}
+
+//hold game function Player 2
+function holdgame2_clicked() {
+  if (player2_playing == true) {
+    console.log('player 1 turn to play')
+    player1_playing = true;
+    player2_playing = false;
+  }
 }
